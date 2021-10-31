e41cdf670282ae9bae1b15ed18f8dc4497cd6c88
anandarpit <name@email.com>
2021-10-31 19:33:46 +0530

    ..

--- src/User.js
+++ src/User.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "./Login.css";
-import zipTargetFiles from "../node_modules/jszip"
 import { Link } from "react-router-dom";
+var base64 = require("file-base64");
 
 // aadharNumber = 999989525451
+
 function User() {
   var base64 = require('file-base64');
   const [aadharNumber, setAadharNumber] = React.useState("");
@@ -23,59 +24,42 @@ function User() {
     }).then(console.log(aadharNumber));
   };
 
+  const downloadfile = () => {
+    fetch("https://stage1.uidai.gov.in/eAadhaarService/api/downloadOfflineEkyc", {
+      method: "POST",
+      body: JSON.stringify({
+        txnNumber: "0acbaa8b-b3ae-433d-a5d2-51250ea8e970",
+        otp: OTP,
+        shareCode: "4567",
+        uid: aadharNumber,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    }).then(response => response.json())
+      .then(data => {
+
+        const base64String = data.eKycXML
+        if (data.status === "Success") {
+          const base64String = data.eKycXML
+          base64.decode(base64String, 'textnuhew.zip', function (err, output) {
+            console.log('success');
+          });
+
+        } else {
+          console.log("incorrect otp")
+        }
+      })
+  };
 
 
-
-    
-    const downloadfile = () => {
-        fetch("https://stage1.uidai.gov.in/eAadhaarService/api/downloadOfflineEkyc", {
-          method: "POST",
-          body: JSON.stringify({
-            txnNumber: "0acbaa8b-b3ae-433d-a5d2-51250ea8e970",
-            otp: OTP,
-            shareCode: "4567",
-            uid: aadharNumber,
-           }),
-          headers: {
-            "Content-type": "application/json; charset=UTF-8",
-          },
-        }).then(response => response.json())
-        .then(data => {
-
-          
-          if (data.status === "Success") {
-            console.log("correct otp")
-            console.log(data.eKycXML);
-            
-
-            var fs = require("fs");
-            var base64String = data.eKycXML ;
-            fs.writeFile("package1.zip", base64String,{encoding:'base64'}, function(err){
-                if(err){
-                    console.log(err);
-                }else{
-                    console.log("Working"); 
-
-                }
-            })   
-          } else {
-            console.log("incorrect otp")
-            // console.log(data.eKycXML);
-            
-
-            // data.errCode
-          }
-        })
-      };
-
-
-    return (
-        <div>
-            <div className="login__container">
+  return (
+    <div>
+      <div className="login__container">
         <h1>Sign-in</h1>
         <div className="login__form">
           <div className="login__form__ID">
-            <input className="input" 
+            <input className="input"
               type="number"
               id="aadharNumber"
               placeholder="Enter aadhar number"
@@ -87,7 +71,7 @@ function User() {
             </button>
           </div>
           <div className="login__form__OTP">
-            <input className="input" style={{marginTop:"12px", marginLeft:"2px"}}
+            <input className="input" style={{ marginTop: "12px", marginLeft: "2px" }}
               type="number"
               placeholder="Enter OTP"
               id="OTP"
@@ -98,15 +82,10 @@ function User() {
               Download e-KYC File
             </button>
           </div>
-
-         
-          
         </div>
       </div>
-            
-            
-        </div>
-    )
+    </div>
+  )
 }
 
 export default User;
